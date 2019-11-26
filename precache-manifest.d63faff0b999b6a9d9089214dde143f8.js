8980d0db7629d16926ff5847404f0bc1ea6d963d
Veronika D <donets.veronika@yahoo.com>
2019-11-26 17:32:58 +0200

    Updates

---
+++ precache-manifest.d63faff0b999b6a9d9089214dde143f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874d45e1dec8bff0e7b148dc61397771",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "450eb9f8f355cd4725a7",
+    "url": "/react_todo-app/static/css/main.abae98c4.chunk.css"
+  },
+  {
+    "revision": "2539ec89f1e54b86e139",
+    "url": "/react_todo-app/static/js/2.fb9c9243.chunk.js"
+  },
+  {
+    "revision": "450eb9f8f355cd4725a7",
+    "url": "/react_todo-app/static/js/main.3cb5393a.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
